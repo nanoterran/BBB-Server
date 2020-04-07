91f450b75c867c86c8f53ffef171a86ff84500ff
nanoterran <nanoterran@yahoo.com>
2020-04-06 19:32:34 -0700

    Should be working

--- Server.c
+++ Server.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +24,7 @@ int main(int argc, char *argv[])
 
   if( bind(socket_file_descriptor,(struct sockaddr *)&server , sizeof(server)) < 0)
   {
-    perror("[-] ERROR: Bind Failed");
+    perror("[-] Bind Failed");
     exit(1);
   }
   puts("[+] Done Binding");
@@ -46,6 +48,40 @@ int main(int argc, char *argv[])
   }
 
   puts("[+] Conected");
+
+  int MAX_SIZE = 100;
+  char read_buffer[MAX_SIZE];
+
+  while(1)
+  {
+    int n = read(new_socket, read_buffer, sizeof(read_buffer));
+    if (n < 0){
+      perror("Socket Server: error reading from server socket.");
+    }
+    // read the message from client and copy it in buffer 
+    // read(socket_file_descriptor, read_buffer, sizeof(read_buffer)); 
+
+    // print buffer which contains the client contents 
+    printf("From client: %s\t To client : ", read_buffer); 
+    bzero(read_buffer, MAX_SIZE); 
+
+  //  // copy server message in the buffer 
+    while ((read_buffer[n++] = getchar()) != '\n'); 
+
+    // // and send that buffer to client 
+    write(new_socket, read_buffer, sizeof(read_buffer)); 
+
+    printf("Client sent: %s\n", read_buffer);
+
+    // if msg contains "Exit" then server exit and chat ended. 
+    if (strncmp("exit", read_buffer, 4) == 0) { 
+      printf("Server Exit...\n"); 
+      break; 
+    } 
+  }
+
+  // After chatting close the socket 
+  close(socket_file_descriptor); 
 
   return 0;
 }
